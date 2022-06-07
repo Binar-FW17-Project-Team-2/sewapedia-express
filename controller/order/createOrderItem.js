210e3ae0d063c3379e07a960d7ee0661ed9ab123
heypurnomo <heypurnomo@gmail.com>
2022-06-07 21:51:20 +0700

    fix controller

--- controller/order/createOrderItem.js
+++ controller/order/createOrderItem.js
@@ -3,8 +3,6 @@ const db = require('../../models')
 module.exports = async (req, res, next) => {
   try {
     const { productId, qty, lamaSewa } = req.body
-    if (isNaN(productId))
-      return res.status(400).json({ message: 'produk id harus number' })
     const product = await db.Product.findOne({ where: { id: productId } })
     if (!product) return res.status(400).json({ message: 'product tidak ada' })
     const orderItem = await db.OrderItem.create({
