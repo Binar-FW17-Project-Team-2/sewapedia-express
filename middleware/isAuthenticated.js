03a3be4a83b2ff7dfe780d2bc7a388de94e6ad44
heypurnomo <heypurnomo@gmail.com>
2022-06-08 01:11:59 +0700

    fix: middleware auth

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,14 +1,10 @@
 const { verifyToken } = require('../utils/tokenHandler')
 
 module.exports = (req, res, next) => {
-  try {
-    const { access_token } = req.headers
-    if (!access_token) return res.status(401).json({ message: 'Unauthorized' })
-    const data = verifyToken(access_token)
-    if (!data) return res.status(401).json({ message: 'Unauthorized' })
-    req.user = data
-    next()
-  } catch (error) {
-    next(error)
-  }
+  const { access_token } = req.headers
+  if (!access_token) return res.status(401).json({ message: 'Unauthorized' })
+  const data = verifyToken(access_token)
+  if (!data) return res.status(401).json({ message: 'Unauthorized' })
+  req.user = data
+  next()
 }
